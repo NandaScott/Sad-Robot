6f6f456a005c36eccef394a003034540577daa66
Nanda <nanda1123@gmail.com>
2017-03-25 12:43:04 -0400

    Added 8ball command to RNG

--- cogs/rng.py
+++ cogs/rng.py
@@ -68,6 +68,32 @@ class RNG():
         ])
         await self.bot.say(lenny)
 
+    @commands.command()
+    async def fortune(self):
+        """Works like an 8ball. Ask it a question."""
+        responses = random.choice([
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Nigga no."
+        ])
+        await self.bot.say(responses)
 
 def setup(bot):
     bot.add_cog(RNG(bot))
