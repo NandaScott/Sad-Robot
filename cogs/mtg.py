9d1f8879f36c1f1cffe6058940dd25486452135e
Nanda <nanda1123@gmail.com>
2017-03-27 00:53:04 -0400

    Bot will now fetch cards with single word names.

--- cogs/mtg.py
+++ cogs/mtg.py
@@ -8,16 +8,32 @@ class Arguments(argparse.ArgumentParser):
     def error(self, message):
         raise RuntimeError(message)
 
+
+
+
 class Mtg():
     def __init__(self, bot):
         self.bot = bot
         loop = asyncio.get_event_loop()
         self.session = aiohttp.ClientSession(loop=loop)
 
+    async def get_json(self, url, **kwargs):
+        async with self.session.get(url, **kwargs) as response:
+            assert response.status == 200
+            return await response.read()
+
     @commands.command()
     async def mtg(self, *, message : str):
+        """
+        Fetches MTG cards.
+
+        ?mtg <cardname>
+
+        For now arguments cannot be passed without error.
+        """
 
         parser = Arguments(add_help=False, allow_abbrev=False)
+        parser.add_argument('cardname')
         parser.add_argument('-p', '--price')
         parser.add_argument('-o', '--oracle')
         parser.add_argument('-l', '--legality')
@@ -29,46 +45,40 @@ class Mtg():
         except Exception as e:
             await self.bot.say(str(e))
             return
-
-        async def get_json(self, url, **kwargs):
-            async with self.session.get(url, **kwargs) as response:
-                assert response.status == 200
-                return await response.read()
-
-        cardname = "+".join(re.findall(r"[\w']+|[.,!?;]", message))
+        # cardname = "+".join(re.findall(r"[\w']+|[.,!?;]", message))
 
         if args.fuzzy:
-            data = await self.get_json(url='http://api.scryfall.com/cards/named?', params={'fuzzy':cardname})
+            data = await self.get_json(url='http://api.scryfall.com/cards/named?', params={'fuzzy':args.cardname})
         else:
-            data = await self.get_json(url='http://api.scryfall.com/cards/search', params={'q': cardname})
+            data = await self.get_json(url='http://api.scryfall.com/cards/search', params={'q': args.cardname})
 
         card = json.loads(data.decode('utf-8'))
-        msg = discord.Embed(url=card['scryfall_uri'], color=discord.Color(0x1b6f9))
+        msg = discord.Embed(url=card['data'][0]['scryfall_uri'], color=discord.Color(0x1b6f9))
 
         if args is not None:
-            msg.set_thumbnail(url=card['image_uri'])
+            msg.set_image(url=card['data'][0]['image_uri'])
         else:
-            msg.set_image(url=card['image_uri'])
-        msg.title = "**" + card['name'] + "**"
+            msg.set_thumbnail(url=card['data'][0]['image_uri'])
+        msg.title = "**" + card['data'][0]['name'] + "**"
 
         if args.price:
             price = []
-            if card['usd']:
+            if card['data'][0]['usd']:
                 price.append("**USD**: "+'${:,.2f}'.format(card['usd']))
-            elif card['eur']:
+            elif card['data'][0]['eur']:
                 price.append("**EUR**: "+'€{:,.2f}'.format(card['eur']))
-            elif card['tix']:
+            elif card['data'][0]['tix']:
                 price.append("**TIX**: "+'{:,.2f}'.format(card['tix']))
             " ".join(price)
             msg.set_description(description=price)
 
         if args.oracle:
-            msg.set_description(description=card['oracle_text'])
+            msg.set_description(description=card['data'][0]['oracle_text'])
 
         if args.legality:
             legal_in = []
             not_legal = []
-            for k, v in card['legalities']:
+            for k, v in card['data'][0]['legalities']:
                 if v == "legal":
                     legal_in.append(v)
                 else:
@@ -77,7 +87,7 @@ class Mtg():
             msg.add_field(name="Not Legal", value=not_legal)
 
         if args.buy:
-            for k, v in card['pruchase_uris']:
+            for k, v in card['data'][0]['purchase_uris']:
                 msg.set_description(description="["+k+"]("+v+")")
 
         await self.bot.say(embed=msg)
