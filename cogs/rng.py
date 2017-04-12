46b8d31657eeb01df718ad2baaaa0459f142b593
Nanda <nanda1123@gmail.com>
2017-04-12 02:33:55 -0400

    Created a dictionary for rhero options.

--- cogs/rng.py
+++ cogs/rng.py
@@ -1,5 +1,6 @@
 import random
 import re
+import json
 from discord.ext import commands
 
 class RNG():
@@ -30,33 +31,11 @@ class RNG():
     @commands.command()
     async def rhero(self):
         """For choosing a random Overwatch hero."""
-        heros=random.choice([
-        'Genji. Embrace your inner weeb.',
-        'Mccree. Reach for the sky!',
-        'Pharah. JUSTICE RAINS FROM-AAAUGH!',
-        'Reaper. Mom didn\'t hug me enough.',
-        'Soldier:76. Call of Battlefield, Modern war games.',
-        'Sombra. *Hacks you in spanish*',
-        'Tracer. Cheers love! The cavelry\'s queer!',
-        'Bastion. Beep Boop, fuck the red team.',
-        'Hanzo. SAKE!',
-        'Junkrat. For when you don\'t need to aim.',
-        'Mei. Fuck you.',
-        'Torbjörn. Cause we\'re on attack right?',
-        'Widowmaker. You won\'t switch and you know it.',
-        'D.va. :wink:',
-        'Orisa. Neigh.',
-        'Reinhardt. *Hanzo can you go Rein?*',
-        'Roadhog. One man a-pork-calypse.',
-        'Winston. DICKS OUT FOR HARAMBE XDDDDDD.',
-        'Zarya. CREDIT TO TEAM.',
-        'Ana. Grandma says it\'s naptime.',
-        'Lucio. I\'m existing as best as I can.',
-        'Mercy. Nice team wipe you had there.',
-        'Symmetra. :musical_note: Tunak Tunak Tun Tunak Tunak Tun :musical_note:',
-        'Zenyatta. Show everyone how many balls you have.'
-        ])
-        await self.bot.say(heros)
+        async with open('/lib/temp_data.json') as data_file:
+            data = json.load(data_file)
+            heros = random.sample(data['heros'], 1)
+            await self.bot.say(heros)
+
 
     @commands.command()
     async def lenny(self):
