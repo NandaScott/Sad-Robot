023df4c869993cefac5ac4c93caf62ec8ba0f6ff
Nanda <nanda1123@gmail.com>
2017-04-12 03:53:23 -0400

    Tag function added and SQL database has been sorta populated.

--- cogs/data.py
+++ cogs/data.py
@@ -11,7 +11,7 @@ class data():
     @commands.command(hidden=True)
     @checks.is_owner()
     async def db(self):
-    
+
         db = sqlite3.connect(os.path.dirname(__file__) + "/lib/tags.db")
         cursor = db.cursor()
         try:
@@ -25,10 +25,21 @@ class data():
         db.commit()
         db.close()
 
-    @commands.command
-    async def tag(self):
-        pass
-
+    @commands.command()
+    async def tag(self, *, message : str):
+            """Let's you reference images or quotes using keyword tags."""
+        try:
+            db = sqlite3.connect(os.path.dirname(__file__) + "/lib/tags.db")
+            cursor = db.cursor()
+            cursor.execute('''select url from tag where tag = '%s' ''' % message.lower())
+            tag = cursor.fetchone()
+            msg = discord.Embed(color=discord.Color(0x1b6f9))
+            msg.set_image(url=tag[0])
+            await self.bot.say(embed=msg)
+            db.close()
+        except Exception as e:
+            await self.bot.say(str(e))
+            return
 
 def setup(bot):
     bot.add_cog(data(bot))
