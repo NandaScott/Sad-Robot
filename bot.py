7f2dee1491a66191b3127f172036c7f7e78a0cf3
Nanda Scott <nanda1123@gmail.com>
2018-03-30 02:16:42 +0000

    Added the ability to call cards by using double brackets [[card]].

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord, logging, aiohttp
 from secret import token
 from discord.ext import commands
+import asyncio, time, re
 
 description = '''
 A bot that was made to practice my python and make a cool function for my discord server.
@@ -29,6 +30,50 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
+async def getRequest(url, **kwargs):
+    async with bot.aiohttp_session.get(url, **kwargs) as response:
+        return await response.json()
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    matched = re.findall(r'\[\[\b[\w\s\'\.]+\b\]\]', message.content)
+
+    for name in matched:
+        asyncio.sleep(0.1)
+        startTimer = time.monotonic()
+        card = await getRequest(url='https://api.scryfall.com/cards/named?', params={'fuzzy':name})
+        endTimer = time.monotonic()
+        f = endTimer - startTimer
+
+        if card['object'] == 'error':
+            await bot.send_message(message.channel, re.sub(r'\(|\'|,|\)+', card['details']))
+            continue
+
+        if 'card_faces' in card:
+            for entry in card['card_faces']:
+                msg = discord.Embed(
+                    title="**{}**".format(entry['name']),
+                    url=card['scryfall_uri'],
+                    color=discord.Color(0x1b6f9)
+                )
+                msg.set_image(url=entry['image_uris']['normal'])
+                msg.footer(text='Fetch took: {} seconds.'.format('%.3f' % f))
+                await bot.send_message(message.channel, embed=msg)
+            return
+
+        msg = discord.Embed(
+            title='**{}**'.format(card['name']),
+            url=card['scryfall_uri'],
+            color=discord.Color(0x1b6f9)
+        )
+
+        msg.set_footer(text='Fetch took: {} seconds.'.format('%.3f' % f))
+        msg.set_image(url=card['image_uris']['normal'])
+        await bot.send_message(message.channel, embed=msg)
 
 
 
